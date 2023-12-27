9a8f1b0957f311981bbb0f1f5b628dbb76fd74d1
James Chao (趙福春) <James.Chao@quantatw.com>
2023-12-27 16:14:36 +0800

    init

---
+++ BootstrapBlazor.OnlyServer1/Pages/TableDemo.razor.cs
@@ -0,0 +1,43 @@
+﻿using BootstrapBlazor.Components;
+using BootstrapBlazor.OnlyServer1.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BootstrapBlazor.OnlyServer1.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class TableDemo : ComponentBase
+    {
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
+
+        [Inject]
+        [NotNull]
+        private ITableExport? TableExport { get; set; }
+
+        private readonly ConcurrentDictionary<Foo, IEnumerable<SelectedItem>> _cache = new();
+
+        private IEnumerable<SelectedItem> GetHobbys(Foo item) => _cache.GetOrAdd(item, f => Foo.GenerateHobbys(Localizer));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static IEnumerable<int> PageItemsSource => new int[] { 20, 40 };
+
+        private async Task<bool> OnExportAsync(ITableExportDataContext<Foo> context)
+        {
+            // 自定义导出方法
+            // 通过 context 参数可以自己查询数据进行导出操作
+            // 本例使用 context 传递来的 Rows/Columns 自定义文件名为 Test.xlsx
+            var ret = await TableExport.ExportExcelAsync(context.Rows, context.Columns, "Test.xlsx");
+
+            // 返回 true 时自动弹出提示框
+            return ret;
+        }
+    }
+}
---
+++ BootstrapBlazor.OnlyServer1/Program.cs
@@ -0,0 +1,40 @@
+﻿using BootstrapBlazor.OnlyServer1.Data;
+
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddBootstrapBlazor();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// 增加 Table 数据服务操作类
+builder.Services.AddTableDemoDataService();
+
+// 增加 Table Excel 导出服务
+builder.Services.AddBootstrapBlazorTableExcelExport();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
